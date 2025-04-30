3bb08287a04d494f9b091f48949bedd971f272b2
Hightouch <cetinkaya.ozden@gmail.com>
2025-04-30 20:09:04 +0200

    Created a new server.js file to set up a basic Express server.

---
+++ test/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = 2980;
+
+const distPath = join(__dirname, 'dist');
+app.use(express.static(distPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(distPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}); 
